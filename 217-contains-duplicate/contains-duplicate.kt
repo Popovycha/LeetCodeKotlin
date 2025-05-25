8423ddc259d8786643bff6ce8b70e83abb887b01
Popovycha <artem220397@gmail.com>
2025-05-25 16:30:35 -0400

    Time: 22 ms (65.63%) | Memory: 67.3 MB (18.39%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.kt
+++ 217-contains-duplicate/contains-duplicate.kt
@@ -1,11 +1,17 @@
 class Solution {
     fun containsDuplicate(nums: IntArray): Boolean {
+        //Prioritize Time
         val set = HashSet<Int>()
-
+    
         for(i in nums.indices) {
             if (set.contains(nums[i])) return true
             set.add(nums[i])
         }
+        //Prioritize Space
+        // nums.sort()
+        // for ( i in 0  until nums.size - 1) {
+        //     if( nums[i] == nums[i + 1]) return true
+        // }
 
     return false
     }
