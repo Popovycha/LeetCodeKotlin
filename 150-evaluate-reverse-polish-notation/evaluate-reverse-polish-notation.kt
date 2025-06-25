e162cc5dbd82dfa5efcbeb62981a4ee4c255c54f
Popovycha <artem220397@gmail.com>
2025-06-24 20:48:41 -0400

    Time: 22 ms (40.25%) | Memory: 46.6 MB (64.78%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.kt
@@ -0,0 +1,34 @@
+class Solution {
+    //Time Complexity: O(n)
+    //Space Complexity: O(n)
+    fun evalRPN(tokens: Array<String>): Int {
+        val stack = ArrayDeque<Int>()
+
+        for (token in tokens) {
+            when (token) {
+                "+" -> {
+                    val a = stack.removeLast()
+                    val b = stack.removeLast()
+                    stack.addLast(b + a)
+                }
+                "-" -> {
+                    val a = stack.removeLast()
+                    val b = stack.removeLast()
+                    stack.addLast(b - a)
+                }
+                "*" -> {
+                    val a = stack.removeLast()
+                    val b = stack.removeLast()
+                    stack.addLast(b * a)
+                }
+                "/" -> {
+                    val a = stack.removeLast()
+                    val b = stack.removeLast()
+                    stack.addLast(b / a)
+                }
+                else -> stack.addLast(token.toInt())
+            }
+        }
+        return stack.last()
+    }
+}
