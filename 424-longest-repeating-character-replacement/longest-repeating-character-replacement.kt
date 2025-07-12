ee425a9033e26b9b2aa43b563e4e0a77e4e3f236
Popovycha <artem220397@gmail.com>
2025-07-12 13:52:43 -0400

    Time: 45 ms (35.08%) | Memory: 46.8 MB (46.60%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+        //Time Complexity O(n) & Space Complexity O(1)
+        val count = mutableMapOf<Char, Int>()
+        var res = 0
+        var l = 0
+        var maxf = 0
+
+        for (r in s.indices) {
+            count[s[r]] = count.getOrDefault(s[r], 0) + 1
+            maxf = maxOf(maxf, count[s[r]]!!)
+
+            while (r - l + 1 - maxf > k) {
+                count[s[l]] = count[s[l]]!! - 1
+                l++
+            }
+            res = maxOf(res, r - l + 1)
+        }
+        return res
+    }
+}
